9bb088cfa618359dae807e02a85781d8cdcae89c
Alex Perez <pruizalex@gmail.com>
2016-03-02 17:02:44 +0100

    Anyadido variable nombre

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,8 @@
 public class helloWorld{
+private static String nombre = "Alex";
 public static void main(String[] args){
 
-System.out.println("Hello World");
+System.out.println("Hola" + nombre);
 }
 }
 
